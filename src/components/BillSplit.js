e325f04ad90783d515c8ee40471780588a98a70a
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-20 08:27:42 +0100

    added JSX to the BillSplit components

--- src/components/BillSplit.js
+++ src/components/BillSplit.js
@@ -1,7 +1,25 @@
+// import Button from "./Button";
+
 const BillSplit = () => {
   return (
-    <div>BillSplit</div>
-  )
-}
+    <div className="form-split-bill">
+      <form>
+        <h2>SPLIT A BILL WITH SARAH</h2>
+        <label>💰Bill value</label>
+        <input type="text" />
+        <label>🧍‍♂️Your expense</label>
+        <input type="text" />
+        <label>🧑‍🤝‍🧑Sarah's expense</label>
+        <input type="text" />
+        <label>🤑Who is paying the bill?</label>
+        <select>
+          <option value="">You</option>
+          <option value="">Sarah</option>
+        </select>
+        {/* <Button>Split bill</Button> */}
+      </form>
+    </div>
+  );
+};
 
-export default BillSplit
+export default BillSplit;
