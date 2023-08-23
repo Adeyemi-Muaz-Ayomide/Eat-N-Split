ba5fe611698524e0e62d1a384170498c855b5949
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-23 16:19:11 +0100

    added interactivity to the splitBill component

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Button from "./Button";
 
 const SplitBill = ({ selectedFriend }) => {
+  const [bill, setBill] = useState("");
+  const [myExpenses, setMyExpenses] = useState("");
+  const friendBill = bill ? bill - myExpenses : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("");
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
       <label>💰 Bill Value</label>
-      <input type="number" />
+      <input
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>🧍‍♂️ Your Expense</label>
-      <input type="number" />
+      <input
+        type="number"
+        value={myExpenses}
+        onChange={(e) =>
+          setMyExpenses(
+            Number(e.target.value) > bill ? myExpenses : Number(e.target.value)
+          )
+        }
+      />
       <label>🧑‍🤝‍🧑 {selectedFriend.name}'s expense:</label>
-      <input type="number" disabled />
+      <input type="number" disabled value={friendBill} />
       <label htmlFor="">🤑Who's paying the bill?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(Number(e.target.value))}
+      >
         <option value="user">You</option>
         <option value={selectedFriend.name}>{selectedFriend.name}</option>
       </select>
